5bb1a6e980fb166e3e3902038e35c69c43eea94c
Kara Bayse <karabayse@gmail.com>
2018-01-19 15:31:42 -0600

    UPDATE statement

--- codecademy.sql
+++ codecademy.sql
@@ -13,3 +13,17 @@ CREATE TABLE celebs (
 );
 
 INSERT INTO celebs (id, name, age) VALUES (1, 'Justin Bieber', 21);
+
+INSERT INTO celebs (id, name, age) VALUES (2, 'Beyonce Knowles', 33);
+
+INSERT INTO celebs (id, name, age) VALUES (3, 'Jeremy Lin', 26);
+
+INSERT INTO celebs (id, name, age) VALUES (4, 'Taylor Swift', 26);
+
+SELECT name FROM celebs;
+
+UPDATE celebs
+SET age = 22
+WHERE id = 1;
+
+SELECT * FROM celebs;
