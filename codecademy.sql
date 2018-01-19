a9418a8e887b1a2e39a2d96008bd1f04a9f3a74e
Kara Bayse <karabayse@gmail.com>
2018-01-19 15:13:34 -0600

    INSERT statement

--- codecademy.sql
+++ codecademy.sql
@@ -5,3 +5,11 @@ CREATE TABLE table_name (
     column_2 data_type,
     column_3 data_type
   );
+
+CREATE TABLE celebs (
+  id INTEGER,
+  name TEXT,
+  age INTEGER
+);
+
+INSERT INTO celebs (id, name, age) VALUES (1, 'Justin Bieber', 21);
