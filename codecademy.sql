c3fa4f4e1467881724b38d2f1d6656f864d2a08d
Kara Bayse <karabayse@gmail.com>
2018-01-19 15:35:27 -0600

    ALTER statement

--- codecademy.sql
+++ codecademy.sql
@@ -1,16 +1,19 @@
 SELECT * FROM celebs;
 
+
 CREATE TABLE table_name (
     column_1 data_type,
     column_2 data_type,
     column_3 data_type
   );
 
+
 CREATE TABLE celebs (
   id INTEGER,
   name TEXT,
   age INTEGER
 );
+
 
 INSERT INTO celebs (id, name, age) VALUES (1, 'Justin Bieber', 21);
 
@@ -22,8 +25,14 @@ INSERT INTO celebs (id, name, age) VALUES (4, 'Taylor Swift', 26);
 
 SELECT name FROM celebs;
 
+
 UPDATE celebs
 SET age = 22
 WHERE id = 1;
+
+SELECT * FROM celebs;
+
+
+ALTER TABLE celebs ADD COLUMN twitter_handle TEXT;
 
 SELECT * FROM celebs;
