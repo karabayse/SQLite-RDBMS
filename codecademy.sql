4eb370a5ee96a607af9b289b1142af1aab47874a
Kara Bayse <karabayse@gmail.com>
2018-02-01 16:32:02 -0600

    LEFT JOINs

--- codecademy.sql
+++ codecademy.sql
@@ -352,3 +352,10 @@ FROM orders
 JOIN subscriptions
 	ON orders.subscription_id = subscriptions.subscription_id
 WHERE subscriptions.description = 'Fashion Magazine';
+
+
+-- LEFT JOINs
+SELECT *
+FROM table1
+LEFT JOIN table2
+  ON table1.c2 = table2.c2
