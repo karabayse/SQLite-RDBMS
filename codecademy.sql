4e554c79252fe4534834f3ba9185d12a6772723b
Kara Bayse <karabayse@gmail.com>
2018-02-07 19:23:11 -0600

    WITH

--- codecademy.sql
+++ codecademy.sql
@@ -438,3 +438,16 @@ FROM newspaper
 UNION
 SELECT *
 FROM online;
+
+
+-- WITH
+WITH previous_query AS (
+SELECT customer_id,
+       COUNT(subscription_id) as subscriptions
+FROM orders
+GROUP BY customer_id)
+SELECT customers.customer_name,
+previous_query.subscriptions
+FROM previous_query
+JOIN customers
+	ON customers.customer_id = previous_query.customer_id;
